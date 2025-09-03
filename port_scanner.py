9bde3935607bf93ebf1b15081995ddb1ce45a1da
Aditya Yadav <adityadav20101609@gmail.com>
2025-09-03 22:34:24 +0530

    Added

--- port_scanner.py
+++ port_scanner.py
@@ -2,7 +2,7 @@ import socket as s
 import sys
 import tqdm as tq
 import threading as th
-s.setdefaulttimeout(7)
+s.setdefaulttimeout(1)
 l=[]
 def scan(ip, port,Version):
     sock=s.socket(Version, s.SOCK_STREAM)
@@ -16,17 +16,17 @@ if(len(sys.argv)<4):
 starting_port=int(sys.argv[2])
 ending_port=int(sys.argv[3])
 ip=sys.argv[1]
-version=s.AF_INET6
+version=s.AF_INET
 if(len(ip.split(":"))==6):
     version=s.AF_INET6
 if((starting_port>=0 and starting_port<65536)*
    (ending_port>=0 and ending_port<65536) and 
    starting_port<=ending_port):
     for i in tq.tqdm(range(starting_port, ending_port+1),desc="Scanning",unit=' port'):
-        #scan(sys.argv[1], i,version)
-        a=th.Thread(target=scan,args=(sys.argv[1], i,version))
-        a.daemon=True
-        a.start()
+        scan(sys.argv[1], i,version)
+        #a=th.Thread(target=scan,args=(sys.argv[1], i,version))
+        #a.daemon=False
+        #a.start()
     print(l)
 else:
     print("Port Number Should be in [0,65535]")
